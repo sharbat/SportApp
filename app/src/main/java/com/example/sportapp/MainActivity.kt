03c090576d88e97660d3bfb9e81adc9bfe7aab62
sharbat <sharbat021515@gmail.com>
2022-09-20 22:09:49 +0600

    SportApp

---
+++ app/src/main/java/com/example/sportapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.sportapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView=findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController=findNavController(R.id.fragment)
+
+        val appBarConfiguration= AppBarConfiguration(setOf(R.id.homeFragment,R.id.profileFragment,R.id.AccountFragment, R.id.SportsFragment))
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
